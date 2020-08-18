1243e4980fe06929d0d9600866064432ff9f29fc
Grinaldi Wisnu <grinaldifoc@gmail.com>
2020-08-19 06:20:28 +0700

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //TODO: create auth API for Hology login and register
+    }
+}
---
+++ app/Http/Controllers/CompetitionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CompetitionController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //TODO: create CRUD for competition and create CRUD for competition by user ID
+    }
+}
