633b984834689987a4350ec08767e53fb2e52f73
afikrim <afikrim10@student.ub.ac.id>
2020-08-28 22:37:40 +0700

    Re assign middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,13 +72,13 @@ $app->configure('app');
 |
 */
 
-$app->middleware([
+// $app->middleware([
+//     App\Http\Middleware\ExampleMiddleware::class
+// ]);
+
+$app->routeMiddleware([
     'auth' => App\Http\Middleware\JwtMiddleware::class
 ]);
-
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
 
 /*
 |--------------------------------------------------------------------------
