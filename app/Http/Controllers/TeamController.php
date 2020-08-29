385e0663976cb9eb680eea705e762ef7a6bcac6d
afikrim <afikrim10@student.ub.ac.id>
2020-08-29 16:46:20 +0700

    Add upload files

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -9,6 +9,7 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\SignatureInvalidException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class TeamController extends Controller
 {
@@ -93,7 +94,7 @@ class TeamController extends Controller
                 'success' => false,
                 'data' => null,
                 'message' => $validation->errors(),
-            ]);
+            ], 403);
         }
 
         // create new team
@@ -118,7 +119,7 @@ class TeamController extends Controller
                 'success' => false,
                 'data' => null,
                 'message' => $e
-            ]);
+            ], 500);
         }
 
         // set relation value
@@ -134,7 +135,7 @@ class TeamController extends Controller
                 'success' => false,
                 'data' => null,
                 'message' => $e
-            ]);
+            ], 500);
         }
 
         $joinToken = $this->encodeToken($relation->team_id);
@@ -201,9 +202,8 @@ class TeamController extends Controller
                 'success' => false,
                 'data' => null,
                 'message' => $e
-            ]);
+            ], 500);
         }
-
 
         return response()->json([
             'success' => true,
@@ -213,36 +213,353 @@ class TeamController extends Controller
     }
 
     /**
+     * get payment proof
+     *
+     * @param smallint $id
+     * @return Illuminate\Http\Response
+     */
+    public function getPaymentProof($id)
+    {
+        // TODO: get team payment proof
+
+        // get team;
+        $team = Team::where('team_id', $id)->get();
+
+        // split ext and filename
+        [$ext, $filename] = explode("-", $team->payment_proof);
+
+
+        // define path
+        $filepath = storage_path("/app/teams/$id/$filename.$ext");
+
+        // read file content
+        try {
+            $file = file_get_contents($filepath);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $e
+            ], 500);
+        }
+
+        return response($file, 200, ['Content-Type' => "image/$ext"]);
+    }
+
+
+    /**
      * Upload payment proof
      *
      * @param Illuminate\Http\Request $request
+     * @param smallint $id
      * @return Illuminate\Http\Response
      */
-    public function uploadPaymentProof(Request $request)
+    public function uploadPaymentProof(Request $request, $id)
     {
         // TODO: Store team payment proof
+
+        // Validate file
+        $validation = Validator::make($request->file(), [
+            'payment_proof' => 'mimes:png,jpg|max:2048'
+        ]);
+
+        // If validation fails
+        if ($validation->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $validation->errors()
+            ], 403);
+        }
+
+        // Get file from request
+        $file = $request->file('payment_proof');
+
+        // Create new filename
+        $fileName = Str::uuid();
+        $ext = $file->getClientOriginalExtension();
+        $filePath = storage_path("/app/teams/$id/$fileName.$ext");
+
+        // Store file in storage
+        move_uploaded_file($file->getPathname(), $filePath);
+
+        // Update field in database;
+        Team::where('team_id', $id)
+            ->update([
+                'team_payment_proof' => "$ext-$fileName"
+            ]);
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'team_payment_proof' => "$ext-$fileName"
+            ],
+            'message' => 'Upload success'
+        ]);
+    }
+
+    /**
+     * get team members identities
+     *
+     * @param smallint $id
+     * @return Illuminate\Http\Response
+     */
+    public function getIdentities($id)
+    {
+        // TODO: get team members identities
+
+        // get relation;
+        $detailTeams = DetailTeam::where('team_id', $id)->get();
+
+        if (!$detailTeams)
+            return response()->json([
+                'success' => 'false',
+                'data' => null,
+                'message' => 'Team not found!'
+            ], 404);
+
+        // define paths
+        $paths = [];
+
+        // fill path with data from table
+        foreach ($detailTeams as $value) {
+            $paths[$value->user_id] = url("api/teams/$id/identity-pics/") . $value->detail_team_identity_pic;
+        }
+
+        // return path to user
+        return response()->json([
+            'success' => true,
+            'data' => $paths,
+            'message' => 'Berhasil menagmbil link untuk identitas user'
+        ]);
+    }
+
+    /**
+     * get team members identity
+     *
+     * @param smallint $id
+     * @param string $filename
+     * @return Illuminate\Http\Response
+     */
+    public function getIdentity($id, $filename)
+    {
+        // TODO: get team members identity
+
+        // get relation;
+        $detailTeam = DetailTeam::where('team_id', $id)
+            ->where('detail_team_identity_pic', $filename)
+            ->get();
+
+        if (!$detailTeam)
+            return response()->json([
+                'success' => 'false',
+                'data' => null,
+                'message' => 'Team not found!'
+            ], 404);
+
+        // split ext and filename
+        [$ext, $filename] = explode("-", $detailTeam->detail_team_identity_pic);
+
+
+        // define path
+        $filepath = storage_path("/app/teams/$id/$filename.$ext");
+
+        // read file content
+        try {
+            $file = file_get_contents($filepath);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $e
+            ], 500);
+        }
+
+        return response($file, 200, ['Content-Type' => "image/$ext"]);
     }
 
     /**
      * Upload identity
      *
      * @param Illuminate\Http\Request $request
+     * @param smallint $id
      * @return Illuminate\Http\Response
      */
-    public function uploadIdentity(Request $request)
+    public function uploadIdentity(Request $request, $id)
     {
         // TODO: Store team identities
+
+        // Validate file
+        $validation = Validator::make($request->file(), [
+            'identity' => 'mimes:png,jpg|max:2048'
+        ]);
+
+        // If validation fails
+        if ($validation->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $validation->errors()
+            ], 403);
+        }
+
+        // Get file from request
+        $file = $request->file('identity');
+
+        // Create new filename
+        $fileName = Str::uuid();
+        $ext = $file->getClientOriginalExtension();
+        $filePath = storage_path("/app/teams/$id/$fileName.$ext");
+
+        // Store file in storage
+        move_uploaded_file($file->getPathname(), $filePath);
+
+        // Update field in database;
+        DetailTeam::where('team_id', $id)
+            ->where('user_id', $request->auth->id)
+            ->update([
+                'detail_team_identity_pic' => "$ext-$fileName"
+            ]);
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'detail_team_identity_pic' => "$ext-$fileName"
+            ],
+            'message' => 'Upload success'
+        ]);
     }
 
     /**
-     * Upload assignment letter
+     * get team members proofs
      *
-     * @param Illuminate\Http\Request $request
+     * @param smallint $id
      * @return Illuminate\Http\Response
      */
-    public function uploadAssingmentLetter(Request $request)
+    public function getProofs($id)
     {
-        // TODO: Store team assignment letter
+        // TODO: get team members proofs
+
+        // get relation;
+        $detailTeams = DetailTeam::where('team_id', $id)->get();
+
+        if (!$detailTeams)
+            return response()->json([
+                'success' => 'false',
+                'data' => null,
+                'message' => 'Team not found!'
+            ], 404);
+
+        // define paths
+        $paths = [];
+
+        // fill path with data from table
+        foreach ($detailTeams as $value) {
+            $paths[$value->user_id] = url("api/teams/$id/proofs/") . $value->detail_team_proof;
+        }
+
+        // return path to user
+        return response()->json([
+            'success' => true,
+            'data' => $paths,
+            'message' => 'Berhasil menagmbil link untuk identitas user'
+        ]);
+    }
+
+    /**
+     * get team members prood
+     *
+     * @param smallint $id
+     * @param string $filename
+     * @return Illuminate\Http\Response
+     */
+    public function getProof($id, $filename)
+    {
+        // TODO: get team members proof
+
+        // get relation;
+        $detailTeam = DetailTeam::where('team_id', $id)
+            ->where('detail_team_identity_pic', $filename)
+            ->get();
+
+        if (!$detailTeam)
+            return response()->json([
+                'success' => 'false',
+                'data' => null,
+                'message' => 'Team not found!'
+            ], 404);
+
+        // split ext and filename
+        [$ext, $filename] = explode("-", $detailTeam->detail_team_proof);
+
+
+        // define path
+        $filepath = storage_path("/app/teams/$id/$filename.$ext");
+
+        // read file content
+        try {
+            $file = file_get_contents($filepath);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $e
+            ], 500);
+        }
+
+        return response($file, 200, ['Content-Type' => "image/$ext"]);
+    }
+
+    /**
+     * Upload student proof
+     *
+     * @param Illuminate\Http\Request $request
+     * @param smallint $id
+     * @return Illuminate\Http\Response
+     */
+    public function uploadProof(Request $request, $id)
+    {
+        // TODO: Store detail team proof
+
+        // Validate file
+        $validation = Validator::make($request->file(), [
+            'proof' => 'mimes:pdf,png,jpg|max:2048'
+        ]);
+
+        // If validation fails
+        if ($validation->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => $validation->errors()
+            ], 403);
+        }
+
+        // Get file from request
+        $file = $request->file('identity');
+
+        // Create new filename
+        $fileName = Str::uuid();
+        $ext = $file->getClientOriginalExtension();
+        $filePath = storage_path("/app/teams/$id/$fileName.$ext");
+
+        // Store file in storage
+        move_uploaded_file($file->getPathname(), $filePath);
+
+        // Update field in database;
+        DetailTeam::where('team_id', $id)
+            ->where('user_id', $request->auth->id)
+            ->update([
+                'detail_team_proof' => "$ext-$fileName"
+            ]);
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'detail_team_proof' => "$ext-$fileName"
+            ],
+            'message' => 'Upload success'
+        ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -17,6 +19,22 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->post('/{id}/lol/{lol}', function (Request $request, $id, $lol) use ($router) {
+    echo $id, $lol;
+    die();
+
+    $file = $request->file('payment_proof');
+
+    $originalName = $file->getClientOriginalName();
+    $mimeType = $file->getClientMimeType();
+
+    move_uploaded_file($file->getPathname(), storage_path("/app/$originalName"));
+
+    $newFile = file_get_contents(storage_path("/app/$originalName"));
+
+    return response($newFile, 200, ['Content-Type' => $mimeType]);
+});
+
 $router->group(['prefix' => 'api'], function () use ($router) {
     // TODO: Sign routes
 
@@ -30,10 +48,22 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 
     // secure api
     $router->group(['middleware' => 'auth'], function () use ($router) {
+
         // Teams
         $router->get('teams', ['uses' => 'TeamController@index']);
         $router->post('teams', ['uses' => 'TeamController@store']);
-        $router->post('teams/add', ['uses' => 'TeamController@addMember']);
-        
+        $router->post('teams/{id}/add', ['uses' => 'TeamController@addMember']);
+
+        // For team member only
+        $router->group(['middleware' => 'team'], function () use ($router) {
+            $router->get('teams/{id}/payment-proof', ['uses' => 'TeamController@getPaymentProof']);
+            $router->post('teams/{id}/payment-proof', ['uses' => 'TeamController@uploadPaymentProof']);
+            $router->get('teams/{id}/identity-pics', ['uses' => 'TeamController@getIdentities']);
+            $router->get('teams/{id}/identity-pics/{filename}', ['uses' => 'TeamController@getIdentity']);
+            $router->post('teams/{id}/identity-pics', ['uses' => 'TeamController@uploadIdentity']);
+            $router->get('teams/{id}/proofs', ['uses' => 'TeamController@getProofs']);
+            $router->get('teams/{id}/proofs/{filename}', ['uses' => 'TeamController@getProof']);
+            $router->post('teams/{id}/proofs', ['uses' => 'TeamController@uploadProof']);
+        });
     });
 });
