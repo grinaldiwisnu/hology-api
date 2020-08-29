649f86cba9b8c207a0e947b51300c406a965f4d4
afikrim <afikrim10@student.ub.ac.id>
2020-08-29 16:45:57 +0700

    Modify team and detail team tables

---
+++ database/migrations/2020_08_29_085137_move_team_assignment_letter_to_detail_teams.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MoveTeamAssignmentLetterToDetailTeams extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_teams', function (Blueprint $table) {
+            $table->string('detail_team_proof', 45);
+        });
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn('team_assignment_letter');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_teams', function (Blueprint $table) {
+            $table->dropColumn('detail_team_proof');
+        });
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('team_assignment_letter', 45);
+        });
+    }
+}
