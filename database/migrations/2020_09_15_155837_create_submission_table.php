ed5ab4c38143a45a44718e8214d6b5b6725a0c08
Grinaldi Wisnu <grinaldifoc@gmail.com>
2020-09-15 23:03:44 +0700

    add migration for submission

---
+++ database/migrations/2020_09_15_155837_create_submission_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submission', function (Blueprint $table) {
+            $table->smallIncrements('submission_id');
+            $table->unsignedSmallInteger('team_id');
+            $table->string('submission_link');
+            $table->smallInteger('submission_phase');
+            $table->foreign('team_id')->references('team_id')->on('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submission');
+    }
+}
